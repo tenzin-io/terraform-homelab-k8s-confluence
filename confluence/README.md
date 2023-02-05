80ddfe468585432f1f590cab0977b3caf1067679
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-02-04 20:21:16 -0500

    Working Confluece server module

---
+++ confluence/README.md
@@ -0,0 +1,3 @@
+# README
+The Helm chart repo can be found here:
+- <https://github.com/atlassian/data-center-helm-charts>
---
+++ main.tf
@@ -0,0 +1,122 @@
+
+locals {
+  namespace               = "atlassian-system"
+  license_key_secret_name = "confluence-license-secret"
+  postgresql_secret_name  = "postgresql-secret-name"
+}
+
+variable "confluence_license_key" {
+  type        = string
+  default     = null
+  description = "The Confluence license key"
+}
+
+variable "external_domain_name" {
+  type        = string
+  description = "The external domain name to add to host names"
+}
+
+resource "kubernetes_namespace_v1" "atlassian_namespace" {
+  metadata {
+    name = local.namespace
+  }
+}
+
+resource "kubernetes_secret_v1" "confluence_license_secret" {
+  depends_on = [kubernetes_namespace_v1.atlassian_namespace]
+  metadata {
+    name      = local.license_key_secret_name
+    namespace = local.namespace
+  }
+
+  data = {
+    "license-key" = var.confluence_license_key
+  }
+
+}
+
+variable "postgresql_admin_password" {
+  type        = string
+  description = "PostgreSQL admin password"
+}
+
+variable "postgresql_database_name" {
+  type        = string
+  description = "PostgreSQL database name"
+}
+
+
+variable "postgresql_database_username" {
+  type        = string
+  description = "PostgreSQL database user"
+}
+
+variable "postgresql_database_password" {
+  type        = string
+  description = "PostgreSQL database password"
+}
+
+variable "postgresql_replication_password" {
+  type        = string
+  description = "PostgreSQL replication password"
+}
+
+resource "kubernetes_secret_v1" "postgresql_auth_secret" {
+  depends_on = [kubernetes_namespace_v1.atlassian_namespace]
+  metadata {
+    name      = local.postgresql_secret_name
+    namespace = local.namespace
+  }
+
+  data = {
+    "postgres-password" : var.postgresql_admin_password
+    "database-username" : var.postgresql_database_username
+    "database-password" : var.postgresql_database_password
+    "replication-password" : var.postgresql_replication_password
+  }
+}
+
+
+resource "helm_release" "confluence_postgresql" {
+  depends_on = [kubernetes_secret_v1.postgresql_auth_secret, kubernetes_namespace_v1.atlassian_namespace]
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "postgresql"
+  version    = "12.1.6"
+  name       = "confluence-postgresql"
+  namespace  = local.namespace
+  values     = [data.template_file.postgresql_values.rendered]
+
+}
+
+data "template_file" "postgresql_values" {
+  template = file("${path.module}/postgresql/values.yaml")
+  vars = {
+    postgresql_secret_name       = local.postgresql_secret_name
+    postgresql_database_username = var.postgresql_database_username
+    postgresql_database_name     = var.postgresql_database_name
+  }
+}
+
+resource "helm_release" "confluence" {
+  depends_on = [helm_release.confluence_postgresql, kubernetes_namespace_v1.atlassian_namespace, kubernetes_secret_v1.confluence_license_secret]
+  repository = "https://atlassian.github.io/data-center-helm-charts"
+  version    = "1.8.1"
+  chart      = "confluence"
+  name       = "confluence-server"
+  namespace  = local.namespace
+
+  values = [data.template_file.confluence_values.rendered]
+}
+
+data "template_file" "confluence_values" {
+  template = file("${path.module}/confluence/values.yaml")
+  vars = {
+    license_key_secret_name     = local.license_key_secret_name
+    confluence_ingress_hostname = "confluence.${var.external_domain_name}"
+
+    postgresql_secret_name       = local.postgresql_secret_name
+    postgresql_database_username = var.postgresql_database_username
+    postgresql_database_name     = var.postgresql_database_name
+    postgresql_service_name      = "confluence-postgresql"
+  }
+}
